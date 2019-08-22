7f6a98f99e23685e4459773f139787db780a3d69
ornel77 <t.ornel@yahoo.fr>
2019-08-22 21:26:56 +0200

    Updates

---
+++ precache-manifest.40a0d642c90017f006f1898cdd0613bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f31e42e4254546bf4242c13a4a8f94a9",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "b0a52e475c11de83b3fa",
+    "url": "/monsters/static/css/main.c9c7f7ea.chunk.css"
+  },
+  {
+    "revision": "c429a589698ec663bba7",
+    "url": "/monsters/static/js/2.c4a20a25.chunk.js"
+  },
+  {
+    "revision": "b0a52e475c11de83b3fa",
+    "url": "/monsters/static/js/main.f483dd02.chunk.js"
+  },
+  {
+    "revision": "68dfed5f83aecb721e81",
+    "url": "/monsters/static/js/runtime~main.fdfe6386.js"
+  }
+]);
